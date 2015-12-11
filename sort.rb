92193112d130e45dc42d97fc73ba6da9ff4d10d3
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2015-12-10 23:25:57 -0800

    Initial Commit

---
+++ sort.rb
@@ -0,0 +1,25 @@
+def bubble_sort (input_arr)
+  length = input_arr.length #Grag how long the array is
+  temp_array = input_arr #Creating temp array to not affect the original.
+  counter = 1
+  
+  temp_array.each_with_index do |current_obj, index|
+    unless index == temp_array.size - counter  #If this isn't the last item we checked.
+      if temp_array[index] > temp_array[index+1] #Check to see if they're in the right order
+        temp_array[index], temp_array[index+1] = temp_array[index+1], temp_array[index] #If they're not, swap em
+      end #ending If
+    else
+      counter += 1 #make the counter go up, so in the unless statement, we don't keep checking finished indices
+      break #stop this iteration
+    end #ending unless
+  end #ending eachLoop
+  
+  puts counter
+  temp_array
+end #ending method define
+
+
+=begin
+  Compare the first 2 objects in the array, then, if 1 is larger than 2, swap em. Do this for the rest of the Array,
+  until we get to the End. Then repeat it, with one less iteration on the elements.
+=end
