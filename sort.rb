3bff4653734125346846d3abfbb1da488c51ee0e
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2015-12-13 21:24:39 -0800

    Set both bubble sort methods complete

--- sort.rb
+++ sort.rb
@@ -1,11 +1,9 @@
-def bubble_sort (input_arr)
-  length = input_arr.length #Grag how long the array is
+def bubble_sort (input_arr) #Grag how long the array is
   temp_array = input_arr #Creating temp array to not affect the original.
-  counter = 1
   
-  (length-1).times do
+  (input_arr.length-1).times do
     temp_array.each_with_index do |current_obj, index|
-      unless index == temp_array.size - counter  #If this isn't the last item we checked.
+      unless index == temp_array.size - 1  #If this isn't the last item we checked.
         if temp_array[index] > temp_array[index+1] #Check to see if they're in the right order
           temp_array[index], temp_array[index+1] = temp_array[index+1], temp_array[index] #If they're not, swap em
         end #ending If
@@ -16,11 +14,19 @@ def bubble_sort (input_arr)
 end #ending method define
 
 
-=begin
-  Compare the first 2 objects in the array, then, if 1 is larger than 2, swap em. Do this for the rest of the Array,
-  until we get to the End. Then repeat it, with one less iteration on the elements.
+def bubble_sort_by (input_arr)
+  temp_array = input_arr #Make temporary
   
-  
-  Currently: Have it working perfectly for initial iteration, now must figure out a way to get it to work repeatedly
-  until sorted fully.
-=end
+  (input_arr.length-1).times do #Loop over it repeatedly
+    temp_array.each_with_index do |current_obj, index| #One iteration
+      unless index == temp_array.size - 1 #Unless it's last
+        outcome = yield(current_obj, temp_array[index+1]) #Check block to see the outcome
+        
+        if outcome > 0 #if positive/if First is larger than second
+          temp_array[index], temp_array[index+1] = temp_array[index+1], temp_array[index]
+        end #Ending if, that checks if it needs to be switched
+      end #Ending unless, that checks if it's the last one
+    end #ending each which ends one iteration
+  end #ending .times which will ensure it is fully sorted
+  temp_array #returns that array
+end
--- sort_spec.rb
+++ sort_spec.rb
@@ -10,3 +10,22 @@ describe "#bubble_sort" do
     expect(bubble_sort([3,2,5,7,1,14,25,23])).to eq [1,2,3,5,7,14,23,25]
   end
 end
+
+
+describe "#bubble_sort_by" do
+  context "when second goes first, ergo Large to small" do
+    it "returns a sorted array in descending order" do
+      expect(bubble_sort_by([5,2,15,7,3]) do |first, second|
+        second <=> first
+      end).to eq [15,7,5,3,2]
+    end
+  end
+  
+  context "when first is before" do
+    it "returns a sorted array in ascending order" do
+      expect(bubble_sort_by([5,2,15,7,3]) do |first, second|
+        first <=> second
+      end).to eq [2,3,5,7,15]
+    end
+  end
+end
